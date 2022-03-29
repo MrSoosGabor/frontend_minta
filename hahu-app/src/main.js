f12eac22215eddc5ca2bbae96277a8f92a565470
soosgabor <malagi@gmail.com>
2022-03-29 11:04:15 +0200

    npm i bootstrap

---
+++ hahu-app/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+const app = createApp(App)
+
+import 'bootstrap/dist/js/bootstrap'
+app.mount('#app')
