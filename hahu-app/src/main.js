a5fa9bb44cbd3eaec281dc27bf86c8c549f12185
soosgabor <malagi@gmail.com>
2022-04-01 14:20:57 +0200

    A get kérés megvalósítása Axios-szal

--- hahu-app/src/main.js
+++ hahu-app/src/main.js
@@ -3,16 +3,27 @@ import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 import "bootstrap/dist/css/bootstrap.css";
+import OpenPage from './components/OpenPage.vue';
+import IngatlanKinalat from './components/IngatlanKinalat.vue';
+
+import VueGoodTablePlugin from 'vue-good-table-next';
+
+// import the styles
+import 'vue-good-table-next/dist/vue-good-table-next.css'
+
+
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: "/", component: OpenPage },
-    { path: "/offers", IngatlanKinalat },
-  ],
+    history: createWebHistory(),
+    routes: [
+        { path: "/", component: OpenPage },
+        { path: "/offers", component: IngatlanKinalat },
+    ],
 });
 
 const app = createApp(App);
+
+app.use(VueGoodTablePlugin);
 app.use(router);
-import "bootstrap/dist/js/bootstrap";
+
 app.mount("#app");
